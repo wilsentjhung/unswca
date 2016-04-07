1d376fc3a5ea2a26758b6c12f2148a40600f6e7c
mikesimarta <michael.simarta@gmail.com>
2016-04-08 01:39:43 +1000

    small changes to script

--- inc/pgsql.php
+++ inc/pgsql.php
@@ -1,11 +1,11 @@
 <?php
-	$aims_db_connection = pg_connect("host=localhost dbname=aims user=wilsentjhung password=icencepi");
+	$aims_db_connection = pg_connect("host=localhost dbname=aims user=mikesimarta password=test");
 	if (!$aims_db_connection) {
 		echo "Error connecting to the AIMS PostgreSQL database!\n";
 		exit;
 	}
 
-	$sims_db_connection = pg_connect("host=localhost dbname=sims user=wilsentjhung password=icencepi");
+	$sims_db_connection = pg_connect("host=localhost dbname=sims user=mikesimarta password=test");
 	if (!$sims_db_connection) {
 		echo "Error connecting to the SIMS PostgreSQL database!\n";
 		exit;
--- script/scraper.py
+++ script/scraper.py
@@ -2,7 +2,7 @@
 
 import urllib2
 import re
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 
 allCourseCode = set()
 codePattern = re.compile(r"[A-Z]{4}[0-9]{4}")
@@ -56,6 +56,7 @@ for hc in subjectCode:
 			#change to ands
 			prereq = re.sub(r"\sAND\s", " && ", prereq, flags=re.IGNORECASE)
 			prereq = re.sub(r"\s&\s", " && ", prereq, flags=re.IGNORECASE)
+			prereq = re.sub(r"\sincluding\s", " && ", prereq, flags=re.IGNORECASE)
 
 			#comma can mean and/or
 			prereq = re.sub(r",\s*or", " || ", prereq, flags=re.IGNORECASE)
@@ -78,6 +79,21 @@ for hc in subjectCode:
 			prereq = re.sub(r'\[', '(', prereq, flags=re.IGNORECASE)
 			prereq = re.sub(r'\]', ')', prereq, flags=re.IGNORECASE)
 
+
+
+			prereq = re.sub(r'Enrolment in [^(]+ \(([^)]+)\)', r'\1', prereq, flags=re.IGNORECASE)
+			prereq = re.sub(r'approval from the School', "SCHOOL_APPROVAL", prereq, flags=re.IGNORECASE)
+			prereq = re.sub(r'school approval', "SCHOOL_APPROVAL", prereq, flags=re.IGNORECASE)
+			prereq = re.sub(r'Enrolment in Program 3586 && 3587 && 3588 && 3589 && 3155 && 3154 or 4737', "3586 || 3587 || 3588 || 3589 || 3155 || 3154 || 4737", prereq, flags=re.IGNORECASE)
+			prereq = re.sub(r'Enrolment in program ([0-9]+)', r'\1', prereq, flags=re.IGNORECASE)
+
+			prereq = re.sub(r'and in any of the following plans MATHR13986, MATHR13523, MATHR13564, MATHR13956, MATHR13589, MATHR13761, MATHR13946, MATHR13949 \|\| MATHR13998', "(MATHR13986 || MATHR13523 || MATHR13564 || MATHR13956 || MATHR13589 || MATHR13761 || MATHR13946 || MATHR13949 || MATHR13998)", prereq, flags=re.IGNORECASE)
+			prereq = re.sub(r'A pass in BABS1201 plus either a pass in', "BABS1201 && (", prereq, flags=re.IGNORECASE)
+			prereq = re.sub(r'a minimum of a credit in ([A-Za-z]{4}[0-9]{4})', r'\1{CR}', prereq, flags=re.IGNORECASE)
+			prereq = re.sub(r'([0-9]+)\s+UOC\s+at Level 1', r'\1_UOC_LEVEL_1', prereq, flags=re.IGNORECASE)
+
+			prereq = re.sub(r'stream', '', prereq, flags=re.IGNORECASE)
+
 			#cleanup
 			prereq = re.sub(r'&&\s*&&$', '&&', prereq, flags=re.IGNORECASE)
 			prereq = re.sub(r'&&\s*$', ' ', prereq, flags=re.IGNORECASE)
@@ -88,7 +104,22 @@ for hc in subjectCode:
 			prereq = re.sub(r'\s*\)', ')', prereq, flags=re.IGNORECASE)
 			prereq = re.sub(r'\s\s+', ' ', prereq, flags=re.IGNORECASE)
 			prereq = re.sub(r'\'', '\'\'', prereq, flags=re.IGNORECASE)
+			prereq = re.sub(r'^\s+$', '', prereq, flags=re.IGNORECASE)
 			#print prereq
+
+
+			#manual
+			if (codeInUrl[0] ==  "ARCH1302"):
+				prereq = "(ARCH1202 && ARCH1301)"
+			elif (codeInUrl[0] ==  "ARCH1395"):
+				prereq = "(ARCH1394 && ARCH1384)"
+			elif (codeInUrl[0] == "ARTS2006"):
+				prereq = "(MUSC1704 || MUSC1705 || MUSC1706 || ARTS1005 || 3425 || 3426 || 3427 || 3448 || 3449)"
+
+
+
+
+			
 
 			f.write("INSERT INTO pre_reqs (course_code, career, pre_req_condtions, norm_pre_req_conditions) SELECT \'%s\', \'%s\', \'%s\', \'%s\' WHERE NOT EXISTS (SELECT course_code, career FROM pre_reqs WHERE course_code = \'%s\' and career = \'%s\'); \n" % (codeInUrl[0], career, prereqCondition, prereq, codeInUrl[0], career))
          
